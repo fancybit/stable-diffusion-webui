4aa66a2378e927dcc8fa12a52f686d90af593e75
fancybit <fancybit@qq.com>
2023-01-25 17:26:09 +0800

    开启插件安装

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 models_path="/root/autodl-nas/sd/models/"
-./webui.sh --server-name="0.0.0.0" --port=6006 --api --listen
+./webui.sh --server-name="0.0.0.0" --port=6006 --api --listen --enable-insecure-extension-access
 #--nowebui
